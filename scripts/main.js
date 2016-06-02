079917bb75ecca2f0afa246c80b19666580c08bb
Matt Chastain <matthewdchastain@gmail.com>
2016-06-02 17:55:11 -0400

    Added skills

--- scripts/main.js
+++ scripts/main.js
@@ -22,5 +22,28 @@ $(document).on('ready', function() {
 	});
 	$('#myModal').on('shown.bs.modal', function () {
 	  $('#myInput').focus()
-	})
+	});
+	$(function () {
+	    // Get the context of the canvas element we want to select
+	    var data = {
+		    labels: ["JavaScript/ES6", "NodeJS/Express", "Backbone/Marionette", "ReactJS", "CSS3", "CSS pre-processor", "HTML5", "Design"],
+		    datasets: [
+		        {
+		            fillColor: "rgba(151,187,205,0.2)",
+		            strokeColor: "rgba(151,187,205,1)",
+		            pointColor: "rgba(151,187,205,1)",
+		            pointStrokeColor: "#fff",
+		            pointHighlightFill: "#fff",
+		            pointHighlightStroke: "rgba(151,187,205,1)",
+		            data: [75, 40, 70, 45, 50, 55, 60, 25]
+		        }
+		    ]
+		};
+		var option = {
+		    responsive: true,
+		};
+	    var ctx = document.getElementById("skillsChart").getContext('2d');
+	    var myRadarChart = new Chart(ctx).Radar(data, option);
+	    // document.getElementById('skillsChart').innerHTML = ctx;
+	});
 });
